f92a48c2bede03f33450ba02589b9805773adbb4
Kawaiier <smanvelov@gmail.com>
2020-04-10 20:49:32 +0400

    Lesson 3 uploaded

---
+++ lesson-3/script.js
@@ -0,0 +1,13 @@
+function tellFortune() {
+    randomNumber = Math.round(Math.random());
+    if (randomNumber == 0){
+        alert("Да!");
+    }
+    if (randomNumber == 1){
+        alert("Нет!");
+    }
+}
+
+function sayHello() {
+    alert("Приветик!");
+}
